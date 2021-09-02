d73e45edeeaf2afa1a09dfb2bb37829449ba77d8
Amirhb2000 <amirbaghestani2000@gmail.com>
2021-09-02 12:49:10 +0430

    Initial commit

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-document.querySelector(".get-jokes").addEventListener("click", getJokes);
+const http = new easyHTTP();
+// http.get("https://jsonplaceholder.typicode.com/posts", function (err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
 
-function getJokes(e) {
-  const number = document.querySelector("input[type=number]").value;
-  const xhr = new XMLHttpRequest();
+// Create data
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post",
+};
 
-  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+// Creae post
+// http.post(
+//   "https://jsonplaceholder.typicode.com/posts",
+//   data,
+//   function (err, post) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(post);
+//     }
+//   }
+// );
 
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      let output = ``;
-      if (response.type === "success") {
-        response.value.forEach(function (joke) {
-          output += `<li>${joke.joke}</li>`;
-        });
-      } else {
-        output += "<li>Something goes wrong</li>";
-      }
-      document.querySelector(".jokes").innerHTML = output;
+// Update Post
+// http.put(
+//   "https://jsonplaceholder.typicode.com/posts/1",
+//   data,
+//   function (err, post) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(post);
+//     }
+//   }
+// );
+
+// Delete Post
+http.delete(
+  "https://jsonplaceholder.typicode.com/posts/1",
+  function (err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(response);
     }
-  };
-  xhr.send();
-  e.preventDefault();
-}
+  }
+);
